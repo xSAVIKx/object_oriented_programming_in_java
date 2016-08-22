105533b19ef263f6ffedee91bdc6489d3eef2208
Iurii Sergiichuk <iurii.sergiichuk@gmail.com>
2016-08-22 15:37:06 +0300

    Solved Programming-assignment 3

--- src/module4/EarthquakeMarker.java
+++ src/module4/EarthquakeMarker.java
@@ -4,110 +4,137 @@ import de.fhpotsdam.unfolding.data.PointFeature;
 import de.fhpotsdam.unfolding.marker.SimplePointMarker;
 import processing.core.PGraphics;
 
-/** Implements a visual marker for earthquakes on an earthquake map
- * 
+/**
+ * Implements a visual marker for earthquakes on an earthquake map
+ *
  * @author UC San Diego Intermediate Software Development MOOC team
  * @author Your name here
- *
  */
-public abstract class EarthquakeMarker extends SimplePointMarker
-{
-	
-	// Did the earthquake occur on land?  This will be set by the subclasses.
-	protected boolean isOnLand;
+public abstract class EarthquakeMarker extends SimplePointMarker {
 
-	// SimplePointMarker has a field "radius" which is inherited
-	// by Earthquake marker:
-	// protected float radius;
-	//
-	// You will want to set this in the constructor, either
-	// using the thresholds below, or a continuous function
-	// based on magnitude. 
-  
-	
-	
-	/** Greater than or equal to this threshold is a moderate earthquake */
-	public static final float THRESHOLD_MODERATE = 5;
-	/** Greater than or equal to this threshold is a light earthquake */
-	public static final float THRESHOLD_LIGHT = 4;
+    // Did the earthquake occur on land?  This will be set by the subclasses.
+    protected boolean isOnLand;
 
-	/** Greater than or equal to this threshold is an intermediate depth */
-	public static final float THRESHOLD_INTERMEDIATE = 70;
-	/** Greater than or equal to this threshold is a deep depth */
-	public static final float THRESHOLD_DEEP = 300;
+    // SimplePointMarker has a field "radius" which is inherited
+    // by Earthquake marker:
+    // protected float radius;
+    //
+    // You will want to set this in the constructor, either
+    // using the thresholds below, or a continuous function
+    // based on magnitude.
 
-	// ADD constants for colors
 
-	
-	// abstract method implemented in derived classes
-	public abstract void drawEarthquake(PGraphics pg, float x, float y);
-		
-	
-	// constructor
-	public EarthquakeMarker (PointFeature feature) 
-	{
-		super(feature.getLocation());
-		// Add a radius property and then set the properties
-		java.util.HashMap<String, Object> properties = feature.getProperties();
-		float magnitude = Float.parseFloat(properties.get("magnitude").toString());
-		properties.put("radius", 2*magnitude );
-		setProperties(properties);
-		this.radius = 1.75f*getMagnitude();
-	}
-	
+    /**
+     * Greater than or equal to this threshold is a moderate earthquake
+     */
+    public static final float THRESHOLD_MODERATE = 5;
+    /**
+     * Greater than or equal to this threshold is a light earthquake
+     */
+    public static final float THRESHOLD_LIGHT = 4;
 
-	// calls abstract method drawEarthquake and then checks age and draws X if needed
-	public void draw(PGraphics pg, float x, float y) {
-		// save previous styling
-		pg.pushStyle();
-			
-		// determine color of marker from depth
-		colorDetermine(pg);
-		
-		// call abstract method implemented in child class to draw marker shape
-		drawEarthquake(pg, x, y);
-		
-		// OPTIONAL TODO: draw X over marker if within past day		
-		
-		// reset to previous styling
-		pg.popStyle();
-		
-	}
-	
-	// determine color of marker from depth
-	// We suggest: Deep = red, intermediate = blue, shallow = yellow
-	// But this is up to you, of course.
-	// You might find the getters below helpful.
-	private void colorDetermine(PGraphics pg) {
-		//TODO: Implement this method
-	}
-	
+    /**
+     * Greater than or equal to this threshold is an intermediate depth
+     */
+    public static final float THRESHOLD_INTERMEDIATE = 70;
+    /**
+     * Greater than or equal to this threshold is a deep depth
+     */
+    public static final float THRESHOLD_DEEP = 300;
+
+    // ADD constants for colors
+
+
+    // abstract method implemented in derived classes
+    public abstract void drawEarthquake(PGraphics pg, float x, float y);
+
+
+    // constructor
+    public EarthquakeMarker(PointFeature feature) {
+        super(feature.getLocation());
+        // Add a radius property and then set the properties
+        java.util.HashMap<String, Object> properties = feature.getProperties();
+        float magnitude = Float.parseFloat(properties.get("magnitude").toString());
+        properties.put("radius", 2 * magnitude);
+        setProperties(properties);
+        this.radius = 1.75f * getMagnitude();
+    }
+
+
+    // calls abstract method drawEarthquake and then checks age and draws X if needed
+    public void draw(PGraphics pg, float x, float y) {
+        // save previous styling
+        pg.pushStyle();
+        pg.noStroke();
+        // determine color of marker from depth
+        colorDetermine(pg);
+        // call abstract method implemented in child class to draw marker shape
+        drawEarthquake(pg, x, y);
+
+        drawCross(pg, x, y);
+        // reset to previous styling
+        pg.popStyle();
+    }
+
+    private void drawCross(PGraphics pg, float x, float y) {
+        String age = getAge();
+        float r = getRadius();
+        float r2 = r / 2;
+        pg.stroke(0);
+        pg.fill(0);
+        if ("Past Day".equalsIgnoreCase(age) || "Past Hour".equalsIgnoreCase(age)) {
+            pg.line(x - r2, y - r2, x + r2, y + r2);
+            pg.line(x - r2, y + r2, x + r2, y - r2);
+        } else if ("Past Week".equalsIgnoreCase(age)) {
+            pg.line(x, y - r2, x, y + r2);
+            pg.line(x - r2, y, x + r2, y);
+        }
+    }
+
+    // determine color of marker from depth
+    // We suggest: Deep = red, intermediate = blue, shallow = yellow
+    // But this is up to you, of course.
+    // You might find the getters below helpful.
+    private void colorDetermine(PGraphics pg) {
+        float depth = getDepth();
+        if (depth < THRESHOLD_INTERMEDIATE) {
+            pg.fill(pg.color(255, 255, 0));
+        } else if (depth < THRESHOLD_DEEP) {
+            pg.fill(pg.color(0, 0, 255));
+        } else {
+            pg.fill(pg.color(255, 0, 0));
+        }
+    }
+
 	
 	/*
-	 * getters for earthquake properties
+     * getters for earthquake properties
 	 */
-	
-	public float getMagnitude() {
-		return Float.parseFloat(getProperty("magnitude").toString());
-	}
-	
-	public float getDepth() {
-		return Float.parseFloat(getProperty("depth").toString());	
-	}
-	
-	public String getTitle() {
-		return (String) getProperty("title");	
-		
-	}
-	
-	public float getRadius() {
-		return Float.parseFloat(getProperty("radius").toString());
-	}
-	
-	public boolean isOnLand()
-	{
-		return isOnLand;
-	}
-	
-	
+
+    public float getMagnitude() {
+        return Float.parseFloat(getProperty("magnitude").toString());
+    }
+
+    public String getAge() {
+        return getProperty("age").toString();
+    }
+
+    public float getDepth() {
+        return Float.parseFloat(getProperty("depth").toString());
+    }
+
+    public String getTitle() {
+        return (String) getProperty("title");
+
+    }
+
+    public float getRadius() {
+        return Float.parseFloat(getProperty("radius").toString());
+    }
+
+    public boolean isOnLand() {
+        return isOnLand;
+    }
+
+
 }
